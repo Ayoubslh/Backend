d247282225c5260a468ca684c14b0a698d01f01b
Abd-echafi <abdechafifilali@gmail.com>
2024-12-09 20:05:20 +0100

    mongoConnection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv=require('dotenv');
 const app= require('./app');
-
+const connectDB = require('./utils/connectDB');
+const { connect } = require('mongoose');
 dotenv.config({path:'./config.env'})
 
 
@@ -10,7 +11,13 @@ const port=process.env.PORT;
 
 
 const server =app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
+    try{
+      console.log("server connected ...");
+      connectDB();
+      console.log("connet to DB successe ...");
+    } catch(err){
+      console.log("could not connect ...");
+    }
 
 })
 process.on('unhandledRejection', err => {
@@ -19,4 +26,4 @@ process.on('unhandledRejection', err => {
     server.close(() => {
       process.exit(1);
     });
-  });
+});
